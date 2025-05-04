cf15f584bbc88a79e7c910a35019d357c48479ca
victorhd260876 <victor.huamandiaz@gmail.com>
2025-05-04 00:27:29 -0500

    ejemplo de encapsulamiento de clases

--- ClassSample/Program.cs
+++ ClassSample/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Vivienda vivienda = new Vivienda("Edificio","Av. Lima 333","Lima");
+            Console.WriteLine(vivienda.tipo);
+
+            vivienda.tipo = "Condominio";
+            Console.WriteLine(vivienda.tipo);
         }
     }
 }
---
+++ ClassSample/Vivienda.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSample
+{
+    public class Vivienda
+    {
+    //parametros
+        private string _tipo;
+        public string tipo
+        {
+            get { return _tipo; }
+            set { _tipo = value; }
+        }
+        private string _direccion;
+        public string direccion
+        {
+            get { return _direccion; }
+            set { _direccion = value; }
+        }
+        
+        private string _distrito;
+        public string distrito
+        {
+            get { return _distrito; }
+            set { _distrito = value; }
+        }
+
+        //constructor
+        public Vivienda(string tipo, string direccion, string distrito)
+        {
+            _tipo = tipo;
+            _direccion = direccion;
+            _distrito = distrito;
+
+        }
+
+    }
+}
